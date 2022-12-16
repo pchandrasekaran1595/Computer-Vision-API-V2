14d1cf901c116afa8d6005a95a6ecca2edc7730a
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2022-12-17 01:41:10 +0300

    Update make-env-3.9.bat

--- make-env-3.9.bat
+++ make-env-3.9.bat
@@ -1,1 +1,1 @@
-start /MAX cmd /c "cls && title Prepare Environment && py -3.9 -m venv venv && cd venv/Scripts && activate && cd .. && cd .. && pip install -r requirements.txt"
+start /MAX cmd /c "cls && title Prepare Environment && py -3.9 -m venv venv && cd venv/Scripts && activate && cd .. && cd .. && pip install -r requirements.txt && deactivate && timeout /t 5 /nobreak"
